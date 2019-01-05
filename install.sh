06fce1437de583e1534e91c5d2f841253266d517
Alan <alan@mrvl.io>
2019-01-05 04:08:29 +0000

    Add Docker

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
-echo "no-op"
+sudo apt-get update
+
+sudo apt-get install -y \
+    git \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce
