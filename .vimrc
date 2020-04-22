017d1aac0ae486516014928855a0367c3d420d7a
Alan Marvel <alan@mrvl.io>
2020-04-22 14:10:07 -0400

    Add .vimrc with vim-plug

---
+++ .vimrc
@@ -0,0 +1,13 @@
+" vim-plug https://github.com/junegunn/vim-plug/wiki/tips
+
+if empty(glob('~/.vim/autoload/plug.vim'))
+  silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
+   \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+endif
+
+call plug#begin('~/.vim/plugged')
+
+Plug 'purescript-contrib/purescript-vim'
+
+call plug#end()
--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo apt-get update
+# sudo apt-get update
+# 
+# sudo apt-get install -y \
+#     git \
+#     apt-transport-https \
+#     ca-certificates \
+#     curl \
+#     software-properties-common
+# 
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# 
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+# 
+# sudo apt-get update
+# 
+# sudo apt-get install docker-ce
 
-sudo apt-get install -y \
-    git \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-
-sudo apt-get update
-
-sudo apt-get install docker-ce
+ln -sf $DIR/.vimrc /home/$USER/.vimrc
