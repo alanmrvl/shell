8bbc47614eb94605b23c6b89b4a23710143a125c
Alan Marvel <alan@mrvl.io>
2020-08-14 22:29:49 -0400

    Add tmux, nitrogen

--- install.sh
+++ install.sh
@@ -20,8 +20,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 mkdir -p /home/$USER/.config/i3
+mkdir -p /home/$USER/.config/nitrogen
 
 ln -sf $DIR/.npmrc /home/$USER/.npmrc
 ln -sf $DIR/.vimrc /home/$USER/.vimrc
 ln -sf $DIR/.xinitrc /home/$USER/.xinitrc
 ln -sf $DIR/.config/i3/config /home/$USER/.config/i3/config
+ln -sf $DIR/.config/nitrogen/bg-saved.cfg /home/$USER/.config/nitrogen/bg-saved.cfg
+ln -sf $DIR/.config/nitrogen/nitrogen.cfg /home/$USER/.config/nitrogen/nitrogen.cfg
