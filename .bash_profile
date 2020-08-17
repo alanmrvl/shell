a463616cb08bc423b3ce7caa62755312c33677f2
Alan Marvel <alan@mrvl.io>
2020-08-16 20:36:10 -0400

    Add nightly

---
+++ .bash_profile
@@ -0,0 +1,9 @@
+#
+# ~/.bash_profile
+#
+
+[[ -f ~/.bashrc ]] && . ~/.bashrc
+
+export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/.local/npm/bin:$PATH"
+export PATH="$HOME/.apps/bin:$PATH"
--- install.sh
+++ install.sh
@@ -19,12 +19,22 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+mkdir -p /home/$USER/.apps/bin
 mkdir -p /home/$USER/.config/i3
 mkdir -p /home/$USER/.config/nitrogen
+mkdir -p /home/$USER/.local/share/applications
 
+ln -sf $DIR/.bash_profile /home/$USER/.bash_profile
 ln -sf $DIR/.npmrc /home/$USER/.npmrc
 ln -sf $DIR/.vimrc /home/$USER/.vimrc
 ln -sf $DIR/.xinitrc /home/$USER/.xinitrc
+
 ln -sf $DIR/.config/i3/config /home/$USER/.config/i3/config
 ln -sf $DIR/.config/nitrogen/bg-saved.cfg /home/$USER/.config/nitrogen/bg-saved.cfg
 ln -sf $DIR/.config/nitrogen/nitrogen.cfg /home/$USER/.config/nitrogen/nitrogen.cfg
+
+ln -sf $DIR/.local/applications/firefox-nightly.desktop /home/$USER/.local/share/applications/firefox-nightly.desktop
+
+if test -f "/home/$USER/.apps/firefox-nightly/firefox"; then
+    ln -sf /home/$USER/.apps/firefox-nightly/firefox /home/$USER/.apps/bin/nightly
+fi
