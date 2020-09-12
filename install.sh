04b888907474d33125ec8f9e7fe4cb9cd66aaa55
Alan Marvel <alan@mrvl.io>
2020-09-12 08:28:55 -0400

    Add support for macOS

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 #     apt-transport-https \
 #     ca-certificates \
 #     curl \
-#     software-properties-common
+#     software-propertie-common
 # 
 # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 # 
@@ -17,24 +17,32 @@
 # 
 # sudo apt-get install docker-ce
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    HOMEDIR=Users
+else
+    HOMEDIR=home
+fi
+
+echo "HOMEDIR=$HOMEDIR"
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-mkdir -p /home/$USER/.apps/bin
-mkdir -p /home/$USER/.config/i3
-mkdir -p /home/$USER/.config/nitrogen
-mkdir -p /home/$USER/.local/share/applications
+mkdir -p /$HOMEDIR/$USER/.apps/bin
+mkdir -p /$HOMEDIR/$USER/.config/i3
+mkdir -p /$HOMEDIR/$USER/.config/nitrogen
+mkdir -p /$HOMEDIR/$USER/.local/share/applications
 
-ln -sf $DIR/.bash_profile /home/$USER/.bash_profile
-ln -sf $DIR/.npmrc /home/$USER/.npmrc
-ln -sf $DIR/.vimrc /home/$USER/.vimrc
-ln -sf $DIR/.xinitrc /home/$USER/.xinitrc
+ln -sf $DIR/.bash_profile /$HOMEDIR/$USER/.bash_profile
+ln -sf $DIR/.npmrc /$HOMEDIR/$USER/.npmrc
+ln -sf $DIR/.vimrc /$HOMEDIR/$USER/.vimrc
+ln -sf $DIR/.xinitrc /$HOMEDIR/$USER/.xinitrc
 
-ln -sf $DIR/.config/i3/config /home/$USER/.config/i3/config
-ln -sf $DIR/.config/nitrogen/bg-saved.cfg /home/$USER/.config/nitrogen/bg-saved.cfg
-ln -sf $DIR/.config/nitrogen/nitrogen.cfg /home/$USER/.config/nitrogen/nitrogen.cfg
+ln -sf $DIR/.config/i3/config /$HOMEDIR/$USER/.config/i3/config
+ln -sf $DIR/.config/nitrogen/bg-saved.cfg /$HOMEDIR/$USER/.config/nitrogen/bg-saved.cfg
+ln -sf $DIR/.config/nitrogen/nitrogen.cfg /$HOMEDIR/$USER/.config/nitrogen/nitrogen.cfg
 
-ln -sf $DIR/.local/applications/firefox-nightly.desktop /home/$USER/.local/share/applications/firefox-nightly.desktop
+ln -sf $DIR/.local/applications/firefox-nightly.desktop /$HOMEDIR/$USER/.local/share/applications/firefox-nightly.desktop
 
-if test -f "/home/$USER/.apps/firefox-nightly/firefox"; then
-    ln -sf /home/$USER/.apps/firefox-nightly/firefox /home/$USER/.apps/bin/nightly
+if test -f "/$HOMEDIR/$USER/.apps/firefox-nightly/firefox"; then
+    ln -sf /$HOMEDIR/$USER/.apps/firefox-nightly/firefox /$HOMEDIR/$USER/.apps/bin/nightly
 fi
