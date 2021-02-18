78f2821a69ac339fa672464242c2b9b48aace04a
Alan Marvel <alan@mrvl.io>
2021-02-17 19:20:52 -0500

    Switch to swaywm, fix recursive folder creation

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ DIR="$ROOT_DIR/ln"
 OUT_DIR="$ROOT_DIR/out/ln"
 
 mkdir -p $HOME/.apps/bin
-mkdir -p $HOME/.config/i3
 mkdir -p $HOME/.config/newsboat
 mkdir -p $HOME/.config/nitrogen
+mkdir -p $HOME/.config/sway
 mkdir -p $HOME/.local/share/applications
 
 ln -sf $DIR/.bash_profile $HOME/.bash_profile
@@ -16,10 +16,10 @@ ln -sf $DIR/.vimrc $HOME/.vimrc
 ln -sf $DIR/.xinitrc $HOME/.xinitrc
 ln -sf $DIR/.local/alanmrvl $HOME/.local/alanmrvl
 
-ln -sf $DIR/.config/i3/config $HOME/.config/i3/config
 ln -sf $DIR/.config/newsboat/config $HOME/.config/newsboat/config
 ln -sf $DIR/.config/nitrogen/bg-saved.cfg $HOME/.config/nitrogen/bg-saved.cfg
 ln -sf $DIR/.config/nitrogen/nitrogen.cfg $HOME/.config/nitrogen/nitrogen.cfg
+ln -sf $DIR/.config/sway/config $HOME/.config/sway/config
 
 mkdir -p $OUT_DIR/.local/share/applications
 sed "s/{{HOME}}/${HOME//\//\\/}/" $DIR/.local/share/applications/firefox-nightly.desktop > $OUT_DIR/.local/share/applications/firefox-nightly.desktop
